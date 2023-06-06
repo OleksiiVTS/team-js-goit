87c02f520c4771907e98be8df39d6ca299211699
Kysil Ivan <kysilivan1997@gmail.com>
2023-06-06 23:07:47 +0200

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 // НАгадую як робити імпорт з ваших файлів //
 
 // наприклад:    import './js/buttonup.js';
-
 import './upcoming-this-month/index-up-th-mon.js';
 import './weekly-trends/weekly-trends.js';
 
 import './js-header/header.js';
 import './js-vs/spinner-js.js';
-import './js-vs/load-more.js';
-import './js-vs/filter-library.js';
+// import './js-vs/load-more.js';
+// import './js-vs/filter-library.js';
 import './js-vs/pruklad.js';
+
 import './js-catalog-search/catalog-search.js';
