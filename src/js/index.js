b5f3a2966e1c558b6a9cfa17ec07955ca9f8498e
Kysil Ivan <kysilivan1997@gmail.com>
2023-06-06 23:04:36 +0200

    fix2

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // НАгадую як робити імпорт з ваших файлів //
 
 // наприклад:    import './js/buttonup.js';
+
 import './upcoming-this-month/index-up-th-mon.js';
 import './weekly-trends/weekly-trends.js';
 
