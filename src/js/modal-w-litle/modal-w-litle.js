79a4e9f8bf20347073b94e3cc834b7cbd0935c69
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-06-07 14:36:00 +0300

    +

--- src/js/modal-w-litle/modal-w-litle.js
+++ src/js/modal-w-litle/modal-w-litle.js
@@ -7,12 +7,14 @@ export default function startModalWiNoTreiler() {
       
       refs.closeModalBtn.addEventListener("click", closeModal);
       function toggleModal() {
+        document.body.style.overflow = 'hidden'; 
         refs.modal.classList.toggle("m-w-t-is-hidden");
         document.addEventListener("keydown", Escape);
       }
       function closeModal() {
         refs.modal.classList.toggle("m-w-t-is-hidden");
         document.removeEventListener("keydown", Escape);
+        document.body.style.overflow = 'visible'; 
       }
       function Escape(event) {
         if (event.key === "Escape") closeModal()
