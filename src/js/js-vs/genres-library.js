46bc16df3023fe738cb2878d474369f01198944e
SSSerg05 <ssserg05@gmail.com>
2023-06-08 17:53:22 +0300

    modify library

--- src/js/js-vs/genres-library.js
+++ src/js/js-vs/genres-library.js
@@ -1,3 +1,27 @@
+import GenreList from '../class/GenreList.js';
+
+const cl_Genres = new GenreList({
+  selector: '.select',
+  url: '/genre/movie/list',
+  query: 'language=en',
+});
+cl_Genres.getGenreList();
+
+// convert genres
+function convertId_to_Name(aGenre) {
+  list = cl_Genres.importFromLS();
+  if (!aGenre) { 
+    return
+  }
+
+  const result = aGenre.map(item => {
+    const obj = list.find(el => el.id === item);
+    return obj ? obj.name : null;
+  });
+
+  return result.join(', ');
+}
+
 const boxLibraryCinema = document.querySelector('.library');
 const libraryCinema = JSON.parse(localStorage.getItem('libraryFilms'));
 
@@ -11,20 +35,24 @@ window.addEventListener('click', (event) => {
   }
   return;
 });
+onMarkup(libraryCinema)
 
 // отримуемо потрібний масив даних для розмітки і виводу на сторінку
 function onLibreryFilter(event) {
   const genre = Number(event.currentTarget.value);
-  if (!genre){
+  console.log(genre, isNaN(genre));
+  if (isNaN(genre)){
     onMarkup(libraryCinema)
     return
   }
+  // const filter = libraryCinema.filter(element => {
+  //   return element.genres.some(item => item.id === genre);
 
   const filter = libraryCinema.filter(element => {
-    return element.genres.some(item => item.id === genre);
+    return element.genre_ids.some(item => item === genre);
   });
     
-  console.log(filter);
+  // console.log(filter);
   
   onMarkup(filter);
 }
@@ -62,35 +90,63 @@ function TemplateMovieCard( data ) {
     id
   } = data;
 
-  const aGenres = data.genres.slice(0, 2);
+        let aGenres = null;
+        if (data.genres) { 
+          aGenres = data.genres.map(e => e.name).slice(0, 2).join(', ')
+        } else {
+          aGenres = convertId_to_Name(data.genres_ids)
+        }
 
-  return `<a href="${data}" data-id-movie="${id}">
-  <div class="movie-card overlay-card">
-  <img class="gallery__image" src="${'https://image.tmdb.org/t/p/w400'+poster_path}" alt="${original_title}" loading="lazy"/>
-  <div class="gallery__up_image"></div>
-  <div class="catalog_info">
-    <h2 class="catalog_title">
-    ${title}
-    </h2>
-      <div class="ganres_rating">
-        <p class="catalog_genres">
-        $${aGenres.map(e => e.name).join(', ')} | ${release_date.slice(0, 4)}
-        </p>
-        <div class="rating">
-        <div class="rating__body">
-          <div class="rating__active" style="width: ${vote_average.toFixed(1) * 10}%;"></div>
-          <div class="rating__items">
-            <input type="radio" class="rating__item" name="rating" value="1">
-            <input type="radio" class="rating__item" name="rating" value="2">
-            <input type="radio" class="rating__item" name="rating" value="3">
-            <input type="radio" class="rating__item" name="rating" value="4">
-            <input type="radio" class="rating__item" name="rating" value="5">
+  // const aGenres = data.genres.slice(0, 2);
+
+  return `<a href="" data-id-movie="${id}">
+  <div class="movie-card overlay-card" data-id-movie="${id}">
+      <img class="gallery__image" src="${
+        'https://image.tmdb.org/t/p/w400' + poster_path
+      }" alt="${title ? title : original_title}" loading="lazy"/>
+      <div class="gallery__up_image"></div>
+      <div class="catalog_info">
+        <h2 class="catalog_title">
+        ${title ? title : original_title}
+        </h2>
+          <div class="ganres_rating">
+            <p class="catalog_genres">
+            ${aGenres} | ${release_date.slice(0, 4)}
+            </p>
+            <div class="rating">
+            <div class="rating__body">
+              <div class="rating__active" style="width: ${
+                vote_average.toFixed(1) * 10
+              }%;"></div>
+              <div class="rating__items">
+                <input type="radio" class="rating__item" name="rating" value="1">
+                <input type="radio" class="rating__item" name="rating" value="2">
+                <input type="radio" class="rating__item" name="rating" value="3">
+                <input type="radio" class="rating__item" name="rating" value="4">
+                <input type="radio" class="rating__item" name="rating" value="5">
+              </div>
+            </div>
           </div>
-        </div>
+          </div>
       </div>
       </div>
-  </div>
-  </div>
-  </a>`
+      </a>`;
 }
 
+
+function emptyLibraryMarkup() {
+  if (libraryCinema.length === 0) {
+    document.querySelector('.library-filter').style.display = 'none';
+
+    return `<div class="empty-library"> 
+      <p class="empty-library-text">OOPS...<br/> We are very sorry! <br/> You don’t have any movies in your library.</p>
+      <button type="button" class="button-watch-trailer">
+        <a class="hero-href"  href="../src/partials/catalog/catalog-catalog.html">Search movie</a>
+      </button>
+    </div>`;
+  }
+}
+
+if (boxLibraryCinema) {
+  boxLibraryCinema.insertAdjacentHTML('beforeend', emptyLibraryMarkup());
+}
