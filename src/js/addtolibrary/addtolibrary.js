e41acd58e6e9cf90c513e348ea21ee65d523e39d
makspryme <makspryme17@gmail.com>
2023-06-08 20:14:23 +0300

    library
    Доробив адаптив карток в library

--- src/js/addtolibrary/addtolibrary.js
+++ src/js/addtolibrary/addtolibrary.js
@@ -5,12 +5,9 @@ const boxLibrary = document.querySelector('.library');
 
 const library = JSON.parse(localStorage.getItem('libraryFilms'));
 
-
 loadLibrary(library);
-  boxLibrary.innerHTML = '';
-  boxLibrary.insertAdjacentHTML('beforeend', loadLibrary(library));
-
-
+boxLibrary.innerHTML = '';
+boxLibrary.insertAdjacentHTML('beforeend', loadLibrary(library));
 
 selectEl.addEventListener('input', () => {
   for (const film of library) {
@@ -37,10 +34,6 @@ function loadLibrary(library) {
         popularity,
         genres,
       }) => {
-        if (library.length <= 9) {
-          1
-        }
-
         let nameOfGanre = [];
         // const aGenres = data.genre_ids.slice(0, 2);
         for (const { name } of genres) {
@@ -48,37 +41,40 @@ function loadLibrary(library) {
         }
 
         return `<a href="" data-id-movie="">
-      <div class="movie-card overlay-card">
-      <img class="gallery__image" src="${
-        'https://image.tmdb.org/t/p/w400' + poster_path
-      }" alt="${title}" loading="lazy"/>
-      <div class="gallery__up_image"></div>
-      <div class="catalog_info">
-        <h2 class="catalog_title">
-        ${title}
-        </h2>
-          <div class="ganres_rating">
-            <p class="catalog_genres">
-            ${nameOfGanre.slice(0, 2).join(', ')} | ${release_date.slice(0, 4)}
-            </p>
-            <div class="rating">
-            <div class="rating__body">
-              <div class="rating__active" style="width: ${
-                vote_average.toFixed(1) * 10
-              }%;"></div>
-              <div class="rating__items">
-                <input type="radio" class="rating__item" name="rating" value="1">
-                <input type="radio" class="rating__item" name="rating" value="2">
-                <input type="radio" class="rating__item" name="rating" value="3">
-                <input type="radio" class="rating__item" name="rating" value="4">
-                <input type="radio" class="rating__item" name="rating" value="5">
+        <div class="movie-card overlay-card">
+        <img class="gallery__image" src="${
+          'https://image.tmdb.org/t/p/w400' + poster_path
+        }" alt="${title}" loading="lazy"/>
+        <div class="gallery__up_image"></div>
+        <div class="catalog_info">
+          <h2 class="catalog_title">
+          ${title}
+          </h2>
+            <div class="ganres_rating">
+              <p class="catalog_genres">
+              ${nameOfGanre.slice(0, 2).join(', ')} | ${release_date.slice(
+          0,
+          4
+        )}
+              </p>
+              <div class="rating">
+              <div class="rating__body">
+                <div class="rating__active" style="width: ${
+                  vote_average.toFixed(1) * 10
+                }%;"></div>
+                <div class="rating__items">
+                  <input type="radio" class="rating__item" name="rating" value="1">
+                  <input type="radio" class="rating__item" name="rating" value="2">
+                  <input type="radio" class="rating__item" name="rating" value="3">
+                  <input type="radio" class="rating__item" name="rating" value="4">
+                  <input type="radio" class="rating__item" name="rating" value="5">
+                </div>
               </div>
             </div>
-          </div>
-          </div>
-      </div>
-      </div>
-      </a>`;
+            </div>
+        </div>
+        </div>
+        </a>`;
       }
     )
     .join('');
