0405ae15b0948d507ac96a1515fe922de6316ef2
makspryme <makspryme17@gmail.com>
2023-06-08 20:26:25 +0300

    adaptive library

--- src/js/addtolibrary/addtolibrary.js
+++ src/js/addtolibrary/addtolibrary.js
@@ -9,16 +9,7 @@ loadLibrary(library);
 boxLibrary.innerHTML = '';
 boxLibrary.insertAdjacentHTML('beforeend', loadLibrary(library));
 
-selectEl.addEventListener('input', () => {
-  for (const film of library) {
-    for (const { id } of film.genres) {
-      if (id === Number(selectEl.value)) {
-      }
-    }
-  }
-});
 
-// filter
 
 function loadLibrary(library) {
   const libr = library
@@ -35,10 +26,10 @@ function loadLibrary(library) {
         genres,
       }) => {
         let nameOfGanre = [];
-        // const aGenres = data.genre_ids.slice(0, 2);
         for (const { name } of genres) {
           nameOfGanre.push(name);
         }
+
 
         return `<a href="" data-id-movie="">
         <div class="movie-card overlay-card">
@@ -52,10 +43,7 @@ function loadLibrary(library) {
           </h2>
             <div class="ganres_rating">
               <p class="catalog_genres">
-              ${nameOfGanre.slice(0, 2).join(', ')} | ${release_date.slice(
-          0,
-          4
-        )}
+              ${nameOfGanre.slice(0, 2).join(', ')} | ${release_date.slice(0,4)}
               </p>
               <div class="rating">
               <div class="rating__body">
@@ -81,3 +69,88 @@ function loadLibrary(library) {
 
   return libr;
 }
+
+/////////////////////////////////////////////////////////// FILTER
+
+// selectEl.addEventListener('input', () => {
+//   boxLibrary.insertAdjacentHTML('beforeend', loadFilteredLibrary(library));
+// });
+
+// function loadFilteredLibrary(library) {
+//   boxLibrary.innerHTML = '';
+
+//   const libr = library
+//     .map(
+//       ({
+//         title,
+//         backgroundImage,
+//         overview,
+//         vote_average,
+//         vote_count,
+//         poster_path,
+//         release_date,
+//         popularity,
+//         genres,
+//       }) => {
+//         let nameOfGanre = [];
+//         let idOfGanre = [];
+
+//         for (const { name, id } of genres) {
+//           nameOfGanre.push(name);
+//           idOfGanre.push(id);
+//         }
+
+//         if (idOfGanre.includes(Number(selectEl.value))) {
+//           return `<a href="${{
+//             title,
+//             backgroundImage,
+//             overview,
+//             vote_average,
+//             vote_count,
+//             poster_path,
+//             release_date,
+//             popularity,
+//             genres,
+//           }}" data-id-movie="">
+//       <div class="movie-card overlay-card">
+//       <img class="gallery__image" src="${
+//         'https://image.tmdb.org/t/p/w400' + poster_path
+//       }" alt="${title}" loading="lazy"/>
+//       <div class="gallery__up_image"></div>
+//       <div class="catalog_info">
+//         <h2 class="catalog_title">
+//         ${title}
+//         </h2>
+//           <div class="ganres_rating">
+//             <p class="catalog_genres">
+//             ${nameOfGanre.slice(0, 2).join(', ')} | ${release_date.slice(0, 4)}
+//             </p>
+//             <div class="rating">
+//             <div class="rating__body">
+//               <div class="rating__active" style="width: ${
+//                 vote_average.toFixed(1) * 10
+//               }%;"></div>
+//               <div class="rating__items">
+//                 <input type="radio" class="rating__item" name="rating" value="1">
+//                 <input type="radio" class="rating__item" name="rating" value="2">
+//                 <input type="radio" class="rating__item" name="rating" value="3">
+//                 <input type="radio" class="rating__item" name="rating" value="4">
+//                 <input type="radio" class="rating__item" name="rating" value="5">
+//               </div>
+//             </div>
+//           </div>
+//           </div>
+//       </div>
+//       </div>
+//       </a>`;
+//         }
+//       }
+//     )
+//     .join('');
+
+//   if ((boxLibrary.innerHTML === '')) {
+//     console.log("nema filmu");
+//   }
+
+//   return libr;
+// }
