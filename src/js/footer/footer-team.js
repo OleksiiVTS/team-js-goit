b12010bc10a64b82b28d7407daf0799936964b3a
IraMira333 <irinat333@gmail.com>
2023-06-12 14:43:28 +0300

    Update footer-team.js

--- src/js/footer/footer-team.js
+++ src/js/footer/footer-team.js
@@ -1,14 +1,75 @@
-(() => {
-  const refs = {
-    openFooterTeamLink: document.querySelector('[data-team-open]'),
-    closeFooterTeamBtn: document.querySelector('[data-team-close]'),
-    footerTeam: document.querySelector('[data-team]'),
-    body: document.querySelector('body'),
-    footer: document.querySelector('.footer-container'),
-  };
-  if (refs.footer.classList.contains('footer-fixed')) {
-    refs.footer.classList.remove('footer-fixed');
+const refs = {
+  openFooterTeamLink: document.querySelector('[data-team-open]'),
+  closeFooterTeamBtn: document.querySelector('[data-team-close]'),
+  footerTeam: document.querySelector('[data-team]'),
+  body: document.querySelector('body'),
+  footer: document.querySelector('.footer'),
+};
+// if (refs.footer.classList.contains('footer-fixed')) {
+//   refs.footer.classList.remove('footer-fixed');
+// }
+
+// if (refs.body.clientHeight < window.innerHeight) {
+//   refs.footer.classList.add('footer-fixed');
+// }
+
+setTimeout(() => {
+  if (
+    window.innerWidth <= 320 &&
+    refs.body.clientHeight < window.innerHeight - 109
+  ) {
+    refs.footer.innerHTML = `<div class="container footer-container footer-fixed">
+      <p class="footer-text">&#169 2023 | All Rights Reserved | Developed with
+      <svg class="footer__icon" width="14" height="14">
+        <use href="./images/sprite.svg#icon-heart"></use>
+      </svg>
+      by <a class="footer-link" href="#open" data-team-open>GoIT Students</a></p>
+    </div>`;
+    console.log(`mob`);
+  } else if (
+    window.innerWidth > 320 &&
+    window.innerWidth <= 800 &&
+    refs.body.clientHeight < window.innerHeight - 175
+  ) {
+    refs.footer.innerHTML = `<div class="container footer-container footer-fixed">
+      <p class="footer-text">&#169 2023 | All Rights Reserved | Developed with
+      <svg class="footer__icon" width="14" height="14">
+        <use href="./images/sprite.svg#icon-heart"></use>
+      </svg>
+      by <a class="footer-link" href="#open" data-team-open>GoIT Students</a></p>
+    </div>`;
+    console.log(`tablet`);
+  } else if (
+    window.innerWidth > 800 &&
+    refs.body.clientHeight < window.innerHeight - 197
+  ) {
+    refs.footer.innerHTML = `<div class="container footer-container footer-fixed">
+      <p class="footer-text">&#169 2023 | All Rights Reserved | Developed with
+      <svg class="footer__icon" width="14" height="14">
+        <use href="./images/sprite.svg#icon-heart"></use>
+      </svg>
+      by <a class="footer-link" href="#open" data-team-open>GoIT Students</a></p>
+    </div>`;
+    console.log(`desk`);
+  } else {
+    refs.footer.innerHTML = `<div class="container footer-container">
+      <p class="footer-text">&#169 2023 | All Rights Reserved | Developed with
+      <svg class="footer__icon" width="14" height="14">
+        <use href="./images/sprite.svg#icon-heart"></use>
+      </svg>
+      by <a class="footer-link" href="#open" data-team-open>GoIT Students</a></p>
+    </div>`;
+    console.log(`net`);
   }
+  console.log(
+    refs.body.clientHeight,
+    window.innerHeight,
+    'window.innerWidth=',
+    window.innerWidth,
+    document.documentElement.scrollWidth,
+    window.innerHeight - 175
+  );
+
   refs.openFooterTeamLink.addEventListener('click', openTeamModal);
 
   refs.closeFooterTeamBtn.addEventListener('click', closeTeamModal);
@@ -37,14 +98,4 @@
       closeTeamModal();
     }
   }
-  // if (refs.body.clientHeight < window.innerHeight) {
-  //   refs.footer.classList.add('footer-fixed');
-  // }
-
-  setTimeout(() => {
-    if (refs.body.clientHeight < window.innerHeight) {
-      refs.footer.classList.add('footer-fixed');
-    }
-    console.log(refs.body.clientHeight, window.innerHeight);
-  }, 300);
-})();
+}, 300);
